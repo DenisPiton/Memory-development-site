8cd7dc91ced115cd79046e47bad096b9930279f2
DenisPiton <denfill008@mail.ru>
2025-04-14 20:29:43 +0300

    hosts

--- testing_system/settings.py
+++ testing_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@^c06rx*)xi-qwh-cy$_6p5+x%ijqx69tp^rd325$j0_7kxc@=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000","https://ideni.pythonanywhere.com/"]
+ALLOWED_HOSTS = ["127.0.0.1:8000","https://ideni.pythonanywhere.com/", "ideni.pythonanywhere.com"]
 
 
 # Application definition
